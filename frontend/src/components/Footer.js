f20b1372399752db64c863859ed04c140b9e55ac
Bijaya Shrestha <bijay2402stha@gmail.com>
2023-12-28 21:15:52 +0545

    Frontend completely build

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 
 function Footer() {
   return (
     <footer>
       <Container>
         <Row>
-            <Col className='text-center py-3'>
+            <Col className='text-center py-3'> 
             Copyright &copy; BJShop
             </Col>
         </Row>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,26 @@
 import React from 'react'
-import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 function Header() {
   return (
     <header>
         <Navbar bg='dark' variant='dark' expand="lg" collapseOnSelect>
         <Container>
-            <Navbar.Brand href="/">BJShop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand >BJShop</Navbar.Brand>
+          </LinkContainer>
+            
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-                <Nav.Link href="/login"><i className='fas fa-user'></i>LogIn</Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link ><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+              <Nav.Link ><i className='fas fa-user'></i>LogIn</Nav.Link>
+              </LinkContainer>
+                
             </Nav>
             </Navbar.Collapse>
         </Container>
